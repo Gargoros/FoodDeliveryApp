d1a3dedf8d431ad9409e3b0cb9ebe45ed5a08813
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2024-03-14 20:18:52 +0300

    Created base project, removed storyboard references

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by MIKHAIL ZHACHKO on 14.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
